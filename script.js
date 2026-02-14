d97386e670178bb3926c078eec403aa462f4da9d
Lu1s-png <pipod3545@gmail.com>
2026-02-14 18:37:01 -0500

    divición de productos por genero

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-    const toggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
+// Botón ☰ para abrir/cerrar menú principal
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
 
-    toggle.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-    });
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+// Controlar el submenú de Productos en móviles
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+const dropdown = document.querySelector('.dropdown');
+
+dropdownToggle.addEventListener('click', (e) => {
+  e.preventDefault(); // evita que salte directamente a #productos
+  dropdown.classList.toggle('open');
+});
